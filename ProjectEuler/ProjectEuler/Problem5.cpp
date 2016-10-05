451cca0ae5b351b9bff0c8bccecb137532047bf3
wowsuchnamaste <wowsuchnamaste@users.noreply.github.com>
2016-10-05 20:30:54 +0200

    Solved problem 5.

--- ProjectEuler/ProjectEuler/Problem5.cpp
+++ ProjectEuler/ProjectEuler/Problem5.cpp
@@ -6,7 +6,6 @@ Problem5::Problem5()
 {
 }
 
-
 Problem5::~Problem5()
 {
 }
@@ -17,23 +16,12 @@ void Problem5::Solution()
     // from 1 to 10 without any remainder. What is the smallest positive number
     // that is evenly divisible by all of the numbers from 1 to 20 ?
 
-    const int RangeStart = 1, RangeEnd = 20;
-
-    // create a vector of the numbers we want to find the LCM of
-    std::vector<int> MyPrimeFactors;
-
     // iterate through number range 
     for (int CurrentNumber = RangeStart;
          CurrentNumber <= RangeEnd;
          CurrentNumber++
          )
     {
-        // TODO: find number of common prime factors
-        // Least common denominator is of interest. Use code below to find
-        // all factors. Then multiply each to find to which power the factor
-        // does not exceed the range, i.e. 2^4 etc.
-
-        // TODO: this is possibly not required... study solution.
         // prime factorize each number 
         int DivideThisNumber = CurrentNumber;
         while (DivideThisNumber > 1)
@@ -50,16 +38,29 @@ void Problem5::Solution()
         }
     }
 
-    // sort and count number of factors
+    // sort the vector and remove duplicates
     std::sort(MyPrimeFactors.begin(), MyPrimeFactors.end());
+    auto DuplicatePrimeEntries = std::unique(MyPrimeFactors.begin(), MyPrimeFactors.end());
+    MyPrimeFactors.erase(DuplicatePrimeEntries, MyPrimeFactors.end());
 
-    // TODO calculate the LCM of the results
-    long long LeastCommonMultiple = 0;
- 
-    //std::cout 
-    //    << LeastCommonMultiple << " is the smallest positive number that is"
-    //    << " evenly divisible by all numbers in the range 1 to 20.\n";
-
-    // TODO: Remove this when problem5 is finished
-    std::cout << "\n\n Problem 5 is not complete.\n";
+    // Get the result by multiplying prime factors raised to the power of x 
+    // where x is less than or equal to end of range
+    for (size_t i = 0; i < MyPrimeFactors.size(); i++)
+    {
+        int Multiplier = MyPrimeFactors[i];
+        while (Multiplier <= RangeEnd)
+        {
+            if ((Multiplier * Multiplier) >= RangeEnd)
+            {
+                break;
+            }
+            else { Multiplier *= Multiplier; }
+        }
+        Result *= Multiplier;
+    }
+    
+    std::cout
+        << Result
+        << " is the lowest number evenly divisible by all numbers in"
+        << " the range 1 - 20.\n";
 }
--- ProjectEuler/ProjectEuler/Problem5.h
+++ ProjectEuler/ProjectEuler/Problem5.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <vector>
+
+
 class Problem5
 {
 public:
@@ -8,5 +11,10 @@ public:
     void Solution();
 
 private:
+    const int RangeStart = 1, RangeEnd = 20;
 
+    // vector of numbers we want to find the LCM of
+    std::vector<int> MyPrimeFactors;
+    
+    long long Result = 1;
 };
