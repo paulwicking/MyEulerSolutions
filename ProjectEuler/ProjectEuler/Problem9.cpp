9a115e0214a68e7c4be524168cc550e0b8033882
wowsuchnamaste <wowsuchnamaste@users.noreply.github.com>
2016-10-08 21:07:17 +0200

    Preparing for solution.

--- ProjectEuler/ProjectEuler/Problem9.cpp
+++ ProjectEuler/ProjectEuler/Problem9.cpp
@@ -14,6 +14,12 @@ Problem9::~Problem9()
 
 void Problem9::Solution()
 {
+    // A Pythagorean triplet is a set of three natural numbers, a < b < c, 
+    // for which, a2 + b2 = c2
+    // For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+    // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+    // Find the product abc.
 
     // TODO remove when solved
     std::cout << "Problem is not solved yet.\n";
