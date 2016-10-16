e5dc21c223725afdcf091807dbca5b8a5ee30de3
wowsuchnamaste <wowsuchnamaste@users.noreply.github.com>
2016-10-16 13:41:24 +0200

    One small step further down the path.

--- ProjectEuler/ProjectEuler/Problem9.cpp
+++ ProjectEuler/ProjectEuler/Problem9.cpp
@@ -1,4 +1,4 @@
-#include "Problem9.h"
+﻿#include "Problem9.h"
 #include "StandardHeaders.h"
 
 
@@ -21,6 +21,25 @@ void Problem9::Solution()
     // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
     // Find the product abc.
 
+
+
+    // http://www.matematikk.org/artikkel.html?tid=64751
+
+    // Den endelige løsningen fikk problemet hos Diofant. 
+    // I sin bok "Arithmetika" viser han at alle primitive pytagoreiske tripler 
+    // framkommer ved å velge passende verdier for p og q i formelen:
+    //    (p^2−q^2)^2 + (2pq)^2 = (p^2 + q^2)^2
+    
+    // Vi kan sette opp en liste over pythagoreiske tripler ved å bruke denne 
+    // formelen. 
+    
+    // Triplene skriver vi på formen
+    //    (p^2 − q^2, 2pq, p^2 + q^2)
+    // for forskjellige verdier av p og q.
+    // Et pytagoreisk trippel er primitivt dersom 1 er den største felles 
+    // faktoren til x, y og z.
+
+    
     // TODO remove when solved
     std::cout << "Problem is not solved yet.\n";
 }
